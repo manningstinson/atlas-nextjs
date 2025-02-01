c95ae87cb0f7272c62ac82281699bb9eb34b8310
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-01 16:32:24 +0000

    fixed deployment issue with dynamic routing

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,5 +1,11 @@
 export default async function Page({ params }: { params: { id: string } }) {
   //Simulate Page loading
+
+  type PageProps = {
+    params: {
+      id: string;
+    };
+  };
   await new Promise((r) => setTimeout(r, 3000));
 
   return <div>Topic Page: {params.id}</div>;
