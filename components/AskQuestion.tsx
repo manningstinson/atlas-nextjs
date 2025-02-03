3a4a63be8b816f1307fec9e888154f32e764ab7d
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-03 19:28:44 +0000

    adjusted top

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -6,7 +6,7 @@ export function AskQuestion({ topic }: { topic: string }) {
       <input type="hidden" name="topic_id" value={topic} />
       <input type="text"name="title" className="w-full rounded-md border p-2 pr-28" placeholder="Ask a question..."
         required/>
-      <button className="absolute right-2 mt-1 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
+      <button className="absolute right-2 top-1 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
         Ask
       </button>
     </form>
