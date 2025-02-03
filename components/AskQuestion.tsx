716ee6af27e412fde6673a23a27136d9bebee074
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-03 18:52:27 +0000

    updated Ask a question

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -3,10 +3,12 @@ import { addQuestion } from "@/lib/actions";
 export function AskQuestion({ topic }: { topic: string }) {
   return (
     <form className="relative my-8" action={addQuestion}>
-      <input type="hidden" name="topic_id" value={topic} className="hidden" />
+      <input type="hidden" name="topic_id" value={topic} />
+      <input type="text"name="title" className="w-full rounded-md border p-2 pr-28" placeholder="Ask a question..."
+        required/>
       <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
         Ask
       </button>
     </form>
-    );
-  }
+  );
+}
