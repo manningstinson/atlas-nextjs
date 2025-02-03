639f18a094ead586ec6cd598b33a457e4c9afbb5
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-03 16:54:21 +0000

    added post to the form method

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -1,9 +1,8 @@
-
 import { addQuestion } from "@/lib/actions";
 
 export function AskQuestion({ topic }: { topic: string }) {
   return (
-   <form className="relative my-8" action={addQuestion}>
+   <form className="relative my-8" action={addQuestion} method="post">
      <input type="hidden" name="topic_id" value={topic} className="hidden" />
       <button className="absolute right-2 top-2 flex h-10 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
         Ask
