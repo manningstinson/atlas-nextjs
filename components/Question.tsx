448ca231e69277b4877f8b7f122e51f60ab8b63f
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-10 16:22:02 +0000

    updated app/questions

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,4 +1,5 @@
 import VoteButton from "./VoteButton";
+import Link from "next/link";
 
 type QuestionProps = {
   id: string;
@@ -8,12 +9,14 @@ type QuestionProps = {
 
 export function Question({ id, text, votes }: QuestionProps) {
   return (
-    <div className="flex items-center border-l border-r border-t border-atlas-white-300 p-6 first:rounded-t-md last:rounded-b-md last:border-b">
-      <div className="mr-2 rounded-xl bg-secondary px-2 text-sm text-white">
-        {votes}
+    <Link href={`/ui/questions/${id}`}>
+      <div className="flex items-center border-l border-r border-t border-atlas-white-300 p-6 first:rounded-t-md last:rounded-b-md last:border-b hover:bg-gray-50">
+        <div className="mr-2 rounded-xl bg-secondary px-2 text-sm text-white">
+          {votes}
+        </div>
+        <p className="text w-full text-left font-semibold">{text}</p>
+        <VoteButton id={id} />
       </div>
-      <p className="text w-full text-left font-semibold">{text}</p>
-      <VoteButton id={id} />
-    </div>
+    </Link>
   );
 }
