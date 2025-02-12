9a68f9ade049073071458c95791b657a56ae8e11
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2025-02-12 15:58:35 +0000

    updated question

--- components/AskQuestion.tsx
+++ components/AskQuestion.tsx
@@ -4,7 +4,7 @@ export function AskQuestion({ topic }: { topic: string }) {
   return (
     <form className="relative my-8" action={addQuestion}>
       <input type="hidden" name="topic_id" value={topic} />
-      <input type="text"name="title" className="w-full rounded-md border p-2 pr-28" placeholder="Ask a question..."
+      <input type="text"name="title" className="w-full rounded-md border p-2 py-6 pr-28" placeholder="Ask a question..."
         required/>
       <button className="absolute right-2 top-0 flex h-11 w-24 items-center justify-center rounded-md border bg-secondary px-4 text-lg text-white focus:bg-secondary">
         Ask
